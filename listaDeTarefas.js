e4f83aee3a69a389cd181af5ba71e4f1a22c6ed9
Lucas Corte <corte1994.lc@gmail.com>
2021-08-03 08:42:10 -0300

    implementação encapsulando as funções, com o metodo IIFE

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -1,25 +1,27 @@
-const newItem = document.querySelector('[data-form-button]');
+(()=>{ // IIFE Immediately Invoked Function Expression ou “Função de Invocação Imediata”. Para não ter acesso a regras de negócio
+
+  const newItem = document.querySelector('[data-form-button]');
   
-const createTask = (e) => {
-  e.preventDefault();
-
-  const list = document.querySelector('[data-list]');
-  const input= document.querySelector('[data-form-input]');
-  const value = input.value;
-
-  const li = document.createElement('li');
-
-  const content = `<p class="content">${value}</p>`;
-
-  li.innerHTML = content;
-  li.appendChild(ButtonConcludes());
-  li.classList.add('task');
-  list.appendChild(li);
-
-  value = '';
-}
-
-newItem.addEventListener('click', createTask);
+  const createTask = (e) => {
+    e.preventDefault();
+  
+    const list = document.querySelector('[data-list]');
+    const input= document.querySelector('[data-form-input]');
+    const value = input.value;
+  
+    const li = document.createElement('li');
+  
+    const content = `<p class="content">${value}</p>`;
+  
+    li.innerHTML = content;
+    li.appendChild(ButtonConcludes());
+    li.classList.add('task');
+    list.appendChild(li);
+  
+    value = '';
+  }
+  
+  newItem.addEventListener('click', createTask);
   
   const ButtonConcludes = () => {
     const buttonConcludes = document.createElement('button');
@@ -31,10 +33,12 @@ newItem.addEventListener('click', createTask);
   
     return buttonConcludes;
   }
-
+  
   const concludesTask = (e) => {
     const buttonConcludes = e.target;
     const taskCompleted = buttonConcludes.parentElement;
   
     taskCompleted.classList.toggle('done');
   }
+
+})(); // ativa a função IIFE
